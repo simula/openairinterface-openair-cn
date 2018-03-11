SavoBit/openair-cn-mobility

Add PLMN info to CSR, add APN Restriction IE with default value '1'

src/s11/s11_ie_formatter.c
@@ -1132,6 +1132,44 @@ s11_apn_ie_set (
   return RETURNok;
 }
 
+int
+s11_apn_plmn_ie_set (
+  NwGtpv2cMsgHandleT * msg,
+  const char *apn,
+  const ServingNetwork_t * serving_network)
+{
+  NwRcT                                   rc;
+  uint8_t                                *value;
+  uint8_t                                 apn_length;
+  uint8_t                                *last_size;
+
+  //ServingNetwork_t                       *serving_network = (ServingNetwork_t *) arg;
+  DevAssert (serving_network );
+
+  DevAssert (apn );
+  DevAssert (msg );
+  apn_length = strlen (apn);
+  value = calloc (apn_length + 20, sizeof (uint8_t)); //"default" + neu: ".mncXXX.mccXXX.gprs"
+  last_size = &value[0];
+
+  memcpy(&value[1], apn, apn_length);
+  memcpy(&value[apn_length + 1], ".mnc", 4);
+  value[apn_length + 7] = serving_network->mnc[0] != 0x0f ? serving_network->mnc[0] + '0' : '0';
+  value[apn_length + 6] = serving_network->mnc[1] != 0x0f ? serving_network->mnc[0] + '0' : '0';
+  value[apn_length + 5] = serving_network->mnc[2] != 0x0f ? serving_network->mnc[0] + '0' : '0';
+  memcpy(&value[apn_length + 8], ".mcc", 4);
+  value[apn_length + 14] = serving_network->mcc[0] != 0x0f ? serving_network->mcc[0] + '0' : '0';
+  value[apn_length + 13] = serving_network->mcc[1] != 0x0f ? serving_network->mcc[0] + '0' : '0';
+  value[apn_length + 12] = serving_network->mcc[2] != 0x0f ? serving_network->mcc[0] + '0' : '0';
+  memcpy(&value[apn_length + 15], ".gprs", 5);
+
+  *last_size = apn_length + 19;
+  rc = nwGtpv2cMsgAddIe (*msg, NW_GTPV2C_IE_APN, apn_length + 20, 0, value);
+  DevAssert (NW_OK == rc);
+  free_wrapper ((void**) &value);
+  return RETURNok;
+}
+
 NwRcT
 s11_ambr_ie_get (
   uint8_t ieType,

src/s11/s11_ie_formatter.h
@@ -148,6 +148,8 @@ NwRcT s11_apn_ie_get(
 
 int s11_apn_ie_set(NwGtpv2cMsgHandleT *msg, const char *apn);
 
+int s11_apn_plmn_ie_set(NwGtpv2cMsgHandleT *msg, const char *apn, const ServingNetwork_t * serving_network);
+
 NwRcT s11_ambr_ie_get(
   uint8_t ieType, uint8_t ieLength, uint8_t ieInstance, uint8_t *ieValue, void *arg);
 

src/s11/s11_mme_session_manager.c
@@ -73,6 +73,7 @@ s11_mme_create_session_request (
   s11_rat_type_ie_set (&(ulp_req.hMsg), &req_p->rat_type);
   s11_pdn_type_ie_set (&(ulp_req.hMsg), &req_p->pdn_type);
   s11_paa_ie_set (&(ulp_req.hMsg), &req_p->paa);
+  s11_apn_restriction_ie_set(&(ulp_req.hMsg), 0x01);
   /*
    * Sender F-TEID for Control Plane (MME S11)
    */
@@ -91,7 +92,7 @@ s11_mme_create_session_request (
                               req_p->pgw_address_for_cp.ipv4 ? ntohl(req_p->pgw_address_for_cp.ipv4_address) : 0,
                               req_p->pgw_address_for_cp.ipv6 ? req_p->pgw_address_for_cp.ipv6_address : NULL);
 */
-  s11_apn_ie_set (&(ulp_req.hMsg), req_p->apn);
+  s11_apn_plmn_ie_set (&(ulp_req.hMsg), req_p->apn, &req_p->serving_network);
   s11_serving_network_ie_set (&(ulp_req.hMsg), &req_p->serving_network);
   s11_pco_ie_set (&(ulp_req.hMsg), &req_p->pco);
   for (int i = 0; i < req_p->bearer_contexts_to_be_created.num_bearer_context; i++) {

simula/openairinterface-openair-cn

src/s11/s11_ie_formatter.c
@@ -1518,6 +1518,45 @@ gtpv2c_apn_ie_set (
 }
 
 //------------------------------------------------------------------------------
+int
+gtpv2c_apn_plmn_ie_set (
+    nw_gtpv2c_msg_handle_t * msg,
+  const char *apn,
+  const ServingNetwork_t * serving_network)
+{
+  nw_rc_t                                 rc;
+  uint8_t                                *value;
+  uint8_t                                 apn_length;
+  uint8_t                                *last_size;
+
+  //ServingNetwork_t                       *serving_network = (ServingNetwork_t *) arg;
+  DevAssert (serving_network );
+
+  DevAssert (apn );
+  DevAssert (msg );
+  apn_length = strlen (apn);
+  value = calloc (apn_length + 20, sizeof (uint8_t)); //"default" + neu: ".mncXXX.mccXXX.gprs"
+  last_size = &value[0];
+
+  memcpy(&value[1], apn, apn_length);
+  memcpy(&value[apn_length + 1], ".mnc", 4);
+  value[apn_length + 7] = serving_network->mnc[0] != 0x0f ? serving_network->mnc[0] + '0' : '0';
+  value[apn_length + 6] = serving_network->mnc[1] != 0x0f ? serving_network->mnc[0] + '0' : '0';
+  value[apn_length + 5] = serving_network->mnc[2] != 0x0f ? serving_network->mnc[0] + '0' : '0';
+  memcpy(&value[apn_length + 8], ".mcc", 4);
+  value[apn_length + 14] = serving_network->mcc[0] != 0x0f ? serving_network->mcc[0] + '0' : '0';
+  value[apn_length + 13] = serving_network->mcc[1] != 0x0f ? serving_network->mcc[0] + '0' : '0';
+  value[apn_length + 12] = serving_network->mcc[2] != 0x0f ? serving_network->mcc[0] + '0' : '0';
+  memcpy(&value[apn_length + 15], ".gprs", 5);
+
+  *last_size = apn_length + 19;
+  rc = nwGtpv2cMsgAddIe (*msg, NW_GTPV2C_IE_APN, apn_length + 20, 0, value);
+  DevAssert (NW_OK == rc);
+  free_wrapper ((void**) &value);
+  return RETURNok;
+}
+
+//------------------------------------------------------------------------------
 nw_rc_t
 gtpv2c_ambr_ie_get (
   uint8_t ieType,

src/s11/s11_ie_formatter.h
@@ -148,7 +148,9 @@ nw_rc_t gtpv2c_apn_ie_get(uint8_t ieType, uint8_t ieLength, uint8_t ieInstance, 
 
 int gtpv2c_apn_ie_set(nw_gtpv2c_msg_handle_t *msg, const char *apn);
 
-nw_rc_t gtpv2c_ambr_ie_get(uint8_t ieType, uint8_t ieLength, uint8_t ieInstance, uint8_t *ieValue, void *arg);
+int gtpv2c_apn_plmn_ie_set(nw_gtpv2c_msg_handle_t *msg, const char *apn, const ServingNetwork_t * serving_network);
+
+nw_rc_t gtpv2c_ambr_ie_get (uint8_t ieType, uint8_t ieLength, uint8_t ieInstance, uint8_t * ieValue, void *arg);
 
 nw_rc_t gtpv2c_mei_ie_get(uint8_t ieType, uint8_t ieLength, uint8_t ieInstance, uint8_t *ieValue, void *arg);
 

src/s11/s11_mme_session_manager.c
@@ -91,6 +91,7 @@ s11_mme_create_session_request (
   gtpv2c_rat_type_ie_set (&(ulp_req.hMsg), &req_p->rat_type);
   gtpv2c_pdn_type_ie_set (&(ulp_req.hMsg), &req_p->pdn_type);
   gtpv2c_paa_ie_set (&(ulp_req.hMsg), &req_p->paa);
+  gtpv2c_apn_restriction_ie_set(&(ulp_req.hMsg), 0x01);
 
   /*
    * Sender F-TEID for Control Plane (MME S11)
@@ -110,7 +111,7 @@ s11_mme_create_session_request (
                               req_p->pgw_address_for_cp.ipv4 ? &req_p->pgw_address_for_cp.ipv4_address : 0,
                               req_p->pgw_address_for_cp.ipv6 ? &req_p->pgw_address_for_cp.ipv6_address : NULL);
 */
-  gtpv2c_apn_ie_set (&(ulp_req.hMsg), req_p->apn);
+  gtpv2c_apn_plmn_ie_set (&(ulp_req.hMsg), req_p->apn, &req_p->serving_network);
   gtpv2c_serving_network_ie_set (&(ulp_req.hMsg), &req_p->serving_network);
   gtpv2c_pco_ie_set (&(ulp_req.hMsg), &req_p->pco);
 