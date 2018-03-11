SavoBit/openair-cn-mobility

Add PAA IE to CSR

src/s11/s11_mme_session_manager.c
@@ -72,6 +72,7 @@ s11_mme_create_session_request (
   s11_imsi_ie_set (&(ulp_req.hMsg), &req_p->imsi);
   s11_rat_type_ie_set (&(ulp_req.hMsg), &req_p->rat_type);
   s11_pdn_type_ie_set (&(ulp_req.hMsg), &req_p->pdn_type);
+  s11_paa_ie_set (&(ulp_req.hMsg), &req_p->paa);
   /*
    * Sender F-TEID for Control Plane (MME S11)
    */

simula/openairinterface-openair-cn

src/s11/s11_mme_session_manager.c
@@ -90,6 +90,8 @@ s11_mme_create_session_request (
   gtpv2c_imsi_ie_set (&(ulp_req.hMsg), &req_p->imsi);
   gtpv2c_rat_type_ie_set (&(ulp_req.hMsg), &req_p->rat_type);
   gtpv2c_pdn_type_ie_set (&(ulp_req.hMsg), &req_p->pdn_type);
+  gtpv2c_paa_ie_set (&(ulp_req.hMsg), &req_p->paa);
+
   /*
    * Sender F-TEID for Control Plane (MME S11)
    */